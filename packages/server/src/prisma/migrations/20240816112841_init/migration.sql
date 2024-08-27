2d159a08206291dda47984ad7b14efa04c19f05e
Christoph Könekamp <55452146+thekoenekamp@users.noreply.github.com>
2024-08-28 01:53:44 +0200

    Delete packages/server/src/prisma/migrations directory

--- packages/server/src/prisma/migrations/20240816112841_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `totalUnsettledBalance` to the `ChannelBalance` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "ChannelBalance" ADD COLUMN     "totalUnsettledBalance" INTEGER NOT NULL;
